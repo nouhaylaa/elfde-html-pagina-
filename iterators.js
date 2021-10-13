7025da849573cfcb5e0e09347fded90ffca0d701
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-10-13 19:39:58 +0200

    Add files via upload

---
+++ iterators.js
@@ -0,0 +1,13 @@
+const colorArray = ["yellow", "blue", "red", "orange"];
+
+let i = 0;
+while (i < colorArray.length) {
+    console.log(colorArray[i]);
+    i++;
+}
+
+for (let i = 0; i < colorArray.length; i++) {
+    console.log(colorArray[i]);
+}
+
+colorArray.forEach((color) => console.log(color));
